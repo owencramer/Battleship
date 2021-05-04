2672587467025de47d8da5dc726692843880cbb7
Owen Cramer <owen.cramer@cecstudents.org>
2021-05-04 09:30:56 -0600

    Added Constants, removed printBoardOne, added some logic to Player class.

--- src/Game_Generate.java
+++ src/Game_Generate.java
@@ -4,19 +4,12 @@
 
 public class Game_Generate {
     final private int[] labelsNum = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    final private char[] labelsChar = {'A','B','C','D','E','F','G','H','I','J'};
 
     private void printLine(int dots, boolean newLine) {
         for (int i = 0; i < dots; i++) {
             System.out.print(labelsNum[i] + " * ");
         }
         if (newLine) System.out.println();
-    }
-
-    private void printBoardOne() {
-        for (int i = 12; i > 1; i--) {
-            System.out.print(" * ");
-        }
     }
 
 
--- src/Player.java
+++ src/Player.java
@@ -3,5 +3,38 @@
  * Randomizes battleships on game board
  */
 
+import java.awt.*;
+import java.util.*;
+
 public class Player {
+    private int id;
+    private int lives;
+    Game_Generate board = new Game_Generate();
+    private Map<Point, Boolean> targetHistory;
+    private Scanner scanner;
+
+    public Player (int id) {
+        System.out.printf("%n=== Settings up everything for Player %s ====", id);
+        this.id = id;
+        this.lives = Constants.PLAYER_LIVES;
+        this.targetHistory = new HashMap<>();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void decrementLiveByOne() {
+        lives--;
+    }
+
+    public void playerPlay(Player opponent) {
+        System.out.printf("%n%nPlayer %d, Choose coordinates you want to hit (x, y) ", id);
+        Point point = new Point(scanner.nextInt(), scanner.nextInt());
+    }
 }
