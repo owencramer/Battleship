805fb826fc5f6f9eb9e372b6328a6d4ed0816c59
Mehmet Buegel <mehmetbuegel@gmail.com>
2021-05-06 09:31:08 -0600

    Added constants.

--- src/Ship.java
+++ src/Ship.java
@@ -2,12 +2,6 @@
  * Mehmet, Owen, Max
  */
 
-
 public class Ship {
-    private String name;
-    private int size;
-    private int livesLeft;
-    private boolean isSunk;
-    private Position position;
 
 }
